7755106a382adda47737702ac57be410c3c78b96
Abhishek Yadav <abhismart098@gmail.com>
2023-12-17 13:44:09 +0530

    This is checking task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import ImageSlider from './Componets/imageSlider';
+import image1 from './img/Image1.webp';
+import image2 from './img/images2.jpeg';
+import image3 from './img/Image3.webp';
+
+
+const Images = [
+  { src: image1, alt: 'Image 1' },
+  { src: image2, alt: 'Image 2' },
+  { src: image3, alt: 'Image 3' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageSlider
+        images={Images}
+        parallaxIntensity={0.5} // Adjust as needed
+        transitionSpeed={500} // Adjust as needed
+      />
     </div>
   );
 }
---
+++ src/Componets/imageSlider.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './ImageSlider.css'; // Create this file for custom styles
+import { useSpring, animated } from 'react-spring';
+
+const ImageSlider = ({ images, parallaxIntensity, transitionSpeed }) => {
+  const props = useSpring({
+    transform: `translateY(${parallaxIntensity * 100}px)`,
+    config: { mass: 1, tension: 170, friction: 26 },
+  });
+
+  return (
+    <Carousel
+      showArrows={true}
+      showThumbs={false}
+      infiniteLoop={true}
+      dynamicHeight={false}
+    >
+      {images.map((image, index) => (
+        <div key={index} className="slider-item">
+          <animated.img
+            src={image.src}
+            alt={image.alt}
+            style={{
+              transform: props.transform.interpolate(
+                (value) => `translateY(${value}px)`
+              ),
+              transition: `transform ${transitionSpeed}ms ease-in-out`,
+            }}
+          />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImageSlider;
